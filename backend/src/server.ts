a0c60c931cf8f7ef90f2cb3e39e9b18a2ebf45fa
prajapati bansi <prajapatibansi1510@gmail.com>
2026-02-03 19:30:08 +0530

    frontend setup and authetication complete

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,10 +10,12 @@ import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/user", userRouter);
